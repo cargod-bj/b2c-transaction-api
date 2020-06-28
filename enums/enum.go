0a0c7b4ea54290e44e6b23eb0d2b43b1ade7f57f
ypq <374563453@qq.com>
2020-06-28 14:52:14 +0800

    定义appointment状态

---
+++ enums/enum.go
@@ -0,0 +1,43 @@
+package enums
+
+import "sort"
+
+const (
+	CANCELLED           = 1
+	COMPLETED           = 2
+	CANCELLEDNOTARRIVED = 3
+)
+
+var appointmentStatusText = map[int]string{
+	CANCELLED:           "CANCELLED",
+	COMPLETED:           "COMPLETED",
+	CANCELLEDNOTARRIVED: "CANCELLEDNOTARRIVED",
+}
+
+func AppointmentStatusText(code int) string {
+	return appointmentStatusText[code]
+}
+
+func AppointmentStatusList() []int {
+	return getKeys(appointmentStatusText)
+}
+
+func getKeys(maps map[int]string) []int {
+	keys := make([]int, 0, len(maps))
+	for mapKey, _ := range maps {
+		keys = append(keys, mapKey)
+	}
+	sort.Ints(keys)
+
+	return keys
+}
+
+var appointmentStatusCode = map[string]int{
+	"CANCELLED":           CANCELLED,
+	"COMPLETED":           COMPLETED,
+	"CANCELLEDNOTARRIVED": CANCELLEDNOTARRIVED,
+}
+
+func AppointmentStatusCode(text string) int {
+	return appointmentStatusCode[text]
+}
